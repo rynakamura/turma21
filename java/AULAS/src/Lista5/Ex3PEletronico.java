9cab37599e26c69c4cf276e46cad747c007e9363
Renato-Nakamura <renato.yukio30@hotmail.com>
2021-05-10 18:19:38 -0300

    lista 5

---
+++ java/AULAS/src/Lista5/Ex3PEletronico.java
@@ -0,0 +1,8 @@
+package Lista5;
+
+public class Ex3PEletronico {
+	public static void main(String[] args) {
+		ProdutoEletronico pe1 = new ProdutoEletronico(19.90,"TV","220V",true);
+		System.out.println(pe1.voltagem);
+	}
+}
---
+++ java/AULAS/src/Lista5/ProdutoEletronico.java
@@ -0,0 +1,17 @@
+package Lista5;
+
+public class ProdutoEletronico {
+	public double preco;
+	public String categoria;
+	public String voltagem;
+	public boolean smart;
+	public ProdutoEletronico(double preco, String categoria, String voltagem, boolean smart) {
+		super();
+		this.preco = preco;
+		this.categoria = categoria;
+		this.voltagem = voltagem;
+		this.smart = smart;
+	}
+	
+	
+}
